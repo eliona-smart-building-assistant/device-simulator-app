ee1335d241a305e93f038f6a3b89b52175a0aec2
Zdenek Devaty <zdenek.devaty@eliona.io>
2024-08-14 09:33:36 +0200

    Fix invalid test

--- integration_test.go
+++ integration_test.go
@@ -18,5 +18,5 @@ func TestApp(t *testing.T) {
 func schema(t *testing.T) {
 	t.Parallel()
 
-	assert.SchemaExists(t, "device_simulator", []string{"configuration", "asset"})
+	assert.SchemaExists(t, "device_simulator", []string{"generator"})
 }
